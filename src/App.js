4587a7be71dc9211a02526f6cb1981d18ac0a99f
tretiaki <tretiak.ira1207@gmail.com>
2022-10-18 14:08:32 +0300

    created a button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">
+        <a href="#" className="btn btn-primary center m-5">
+          Push me
+        </a>
+      </header>
     </div>
   );
 }
