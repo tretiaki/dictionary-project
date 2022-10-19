094dfed12cad5089b74bb12aeec91cf0adb8f73e
tretiaki <tretiak.ira1207@gmail.com>
2022-10-19 16:29:40 +0300

    added dictionary api integration🧞‍♂️

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
+
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+  ////https://dictionaryapi.dev/
   function search(event) {
     event.preventDefault();
-    alert(`Searching for "${keyword}" definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
